9c5d842e6df03c10ce984eb79c0029bf2b0a25e9
James Semesky <asemesky@gmail.com>
2016-06-01 22:55:58 -0400

    First commit

---
+++ src/main/webapp/app/app.js
@@ -0,0 +1,59 @@
+(function() {
+	var gem = {
+		name : 'Azurite',
+		price : 2.95
+	};
+	var app = angular.module('store', [ 'nvd3' ]);
+
+	app.controller('StoreController', function($http) {
+		var vm = this;
+		$http.get('https://data.baltimorecity.gov/resource/hyq3-8sxr.json')
+				.then(function(res) {
+					vm.result = res;
+				});
+
+		vm.options = {
+			chart : {
+				type : 'discreteBarChart',
+				height : 450,
+				margin : {
+					top : 20,
+					right : 20,
+					bottom : 60,
+					left : 55
+				},
+				x : function(d) {
+					return d.label;
+				},
+				y : function(d) {
+					return d.value;
+				},
+				showValues : true,
+				valueFormat : function(d) {
+					return d3.format(',.4f')(d);
+				},
+				transitionDuration : 500,
+				xAxis : {
+					axisLabel : 'X Axis'
+				},
+				yAxis : {
+					axisLabel : 'Y Axis',
+					axisLabelDistance : 30
+				}
+			}
+		};
+		vm.data=[{
+		    key: "Cumulative Return",
+		    values: [
+		        { "label" : "A" , "value" : -29.765957771107 },
+		        { "label" : "B" , "value" : 0 },
+		        { "label" : "C" , "value" : 32.807804682612 },
+		        { "label" : "D" , "value" : 196.45946739256 },
+		        { "label" : "E" , "value" : 0.19434030906893 },
+		        { "label" : "F" , "value" : -98.079782601442 },
+		        { "label" : "G" , "value" : -13.925743130903 },
+		        { "label" : "H" , "value" : -5.1387322875705 }
+		    ]
+		}];
+	});
+})();
